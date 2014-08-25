96779b8208ae70b257b146860f6e8a339f84e19e
kgeneral <kgeneral@hanmail.net>
2014-08-25 20:30:43 +0900

    match resultmessages

--- src/actionsniper/Main.java
+++ src/actionsniper/Main.java
@@ -1,7 +1,11 @@
 package actionsniper;
 
+import java.awt.Color;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
+import javax.swing.border.LineBorder;
 
 public class Main {
 
@@ -29,12 +33,30 @@ public class Main {
 	public class MainWindow extends JFrame {
 		
 		private static final long serialVersionUID = -4274409492013695538L;
+		
+		public static final String SNIPER_STATUS_NAME = "sniper status";
+
+		private static final String STATUS_JOINING = "Joining";
+		
+		private final JLabel sniperStatus = createLabel(STATUS_JOINING);
+		
 		public MainWindow() {
 			super("Auction Sniper");
 			setName(MAIN_WINDOW_NAME);
+			add(sniperStatus);
+			pack();
 			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			setVisible(true);
 		}
+
+		private JLabel createLabel(String initialText) {
+			JLabel result = new JLabel(initialText);
+			result.setName(SNIPER_STATUS_NAME);
+			result.setBorder(new LineBorder(Color.BLACK));
+			return result;
+		}
+		
+		
 	}
 
 }
