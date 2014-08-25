4973e586f677c3f40f97cfe0bec046a7f295daef
kgeneral <kgeneral@hanmail.net>
2014-08-25 20:20:53 +0900

    add main window

--- src/actionsniper/Main.java
+++ src/actionsniper/Main.java
@@ -1,14 +1,40 @@
 package actionsniper;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
 public class Main {
 
-	public static final String MAIN_WINDOW_NAME = null;
-	public static final String SNIPER_STATUS_NAME = null;
+	public static final String MAIN_WINDOW_NAME = "Auction Sniper Main";
+	public static final String SNIPER_STATUS_NAME = "sniper status";
 
-	public static void main(String xmppHostname, String sniperId,
-			String sniperPassword, Object itemId) {
-		// TODO Auto-generated method stub
+	private MainWindow ui;
+
+	public Main() throws Exception {
+		startUserInterface();
+	}
+
+	public static void main(String... args) throws Exception {
+		Main main = new Main();
+	}
+
+	private void startUserInterface() throws Exception {
+		SwingUtilities.invokeAndWait(new Runnable() {
+			public void run() {
+				ui = new MainWindow();
+			}
+		});
+	}
+
+	public class MainWindow extends JFrame {
 		
+		private static final long serialVersionUID = -4274409492013695538L;
+		public MainWindow() {
+			super("Auction Sniper");
+			setName(MAIN_WINDOW_NAME);
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			setVisible(true);
+		}
 	}
 
 }
