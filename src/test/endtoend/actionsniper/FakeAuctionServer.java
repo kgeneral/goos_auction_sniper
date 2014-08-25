ecd2ded831db802af189580930289fad110b6c04
kgeneral <kgeneral@hanmail.net>
2014-08-25 14:05:12 +0900

    initial commit

---
+++ src/test/endtoend/actionsniper/FakeAuctionServer.java
@@ -0,0 +1,29 @@
+package test.endtoend.actionsniper;
+
+public class FakeAuctionServer {
+
+	public FakeAuctionServer(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void startSellingItem() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void hasReceivedJoinRequestFrom() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void announceClosed() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
