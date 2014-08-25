af46f273de42365f0de3a96bf4d9ebbbf555ec1d
kgeneral <kgeneral@hanmail.net>
2014-08-25 17:04:25 +0900

    add FakeAuctionServer

--- src/test/endtoend/actionsniper/FakeAuctionServer.java
+++ src/test/endtoend/actionsniper/FakeAuctionServer.java
@@ -1,34 +1,56 @@
 package test.endtoend.actionsniper;
 
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.ChatManagerListener;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+
 public class FakeAuctionServer {
 
-	public FakeAuctionServer(String string) {
-		// TODO Auto-generated constructor stub
+	public static final String ITEM_ID_AS_LOGIN = "auction-%s";
+	public static final String AUCTION_RESOURCE = "Auction";
+	public static final String XMPP_HOSTNAME = "localhost";
+
+	private static final String AUCTION_PASSWORD = "auction";
+
+	private final String itemId;
+	private final XMPPConnection connection;
+
+	private Chat currentChat;
+
+	public FakeAuctionServer(String itemId) {
+		this.itemId = itemId;
+		this.connection = new XMPPConnection(XMPP_HOSTNAME);
 	}
 
-	public void startSellingItem() {
-		// TODO Auto-generated method stub
-		
+	public void startSellingItem() throws XMPPException {
+		connection.connect();
+		connection.login(String.format(ITEM_ID_AS_LOGIN, itemId),
+				AUCTION_PASSWORD, AUCTION_RESOURCE);
+		connection.getChatManager().addChatListener(new ChatManagerListener() {
+			public void chatCreated(Chat chat, boolean createdLocally) {
+				currentChat = chat;
+			}
+		});
+	}
+
+	public Object getItemId() {
+		return itemId;
 	}
 
 	public void hasReceivedJoinRequestFrom() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void announceClosed() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void stop() {
 		// TODO Auto-generated method stub
-		
-	}
 
-	public Object getItemId() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
