f1bb42460d75fb1f6aca9af3f5ade651fb668b80
kgeneral <kgeneral@hanmail.net>
2014-08-25 15:33:00 +0900

    add application runner

---
+++ src/actionsniper/Main.java
@@ -0,0 +1,11 @@
+package actionsniper;
+
+public class Main {
+
+	public static void main(String xmppHostname, String sniperId,
+			String sniperPassword, Object itemId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/test/endtoend/actionsniper/ApplicationRunner.java
+++ src/test/endtoend/actionsniper/ApplicationRunner.java
@@ -1,20 +1,43 @@
 package test.endtoend.actionsniper;
 
+import actionsniper.Main;
+
 public class ApplicationRunner {
 
-	public void startBiddingIn(FakeAuctionServer auction) {
-		// TODO Auto-generated method stub
+	public static final String SNIPER_ID = "sniper"; 
+  	public static final String SNIPER_PASSWORD = "sniper";
+	protected static final String XMPP_HOSTNAME = null;
+	private static final String STATUS_JOINING = null;
+	private static final String STATUS_LOST = null;
+  	private AuctionSniperDriver driver; 
+
+	public void startBiddingIn(final FakeAuctionServer auction) {
+		Thread thread = new Thread("Test Application") {
+			@Override 
+			public void run(){
+				try{
+					Main.main(XMPP_HOSTNAME, SNIPER_ID, SNIPER_PASSWORD, auction.getItemId());
+				} catch (Exception e) {
+					
+				}
+			}
+		};
+		thread.setDaemon(true);
+		thread.start();
+		driver = new AuctionSniperDriver(1000);
 		
+		driver.showsSniperStatus(STATUS_JOINING);
 	}
 
 	public void showsSniperHasLostAuction() {
-		// TODO Auto-generated method stub
+		driver.showsSniperStatus(STATUS_LOST);
 		
 	}
 
 	public void stop() {
-		// TODO Auto-generated method stub
-		
+		if (driver != null) { 
+	      driver.dispose();  
+	    } 
 	}
 
 }
