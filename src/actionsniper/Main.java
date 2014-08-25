2dec3341be689e13304220059dd6b40ca186a144
kgeneral <kgeneral@hanmail.net>
2014-08-25 16:40:16 +0900

    add AuctionSniperDriver

--- src/actionsniper/Main.java
+++ src/actionsniper/Main.java
@@ -2,6 +2,9 @@ package actionsniper;
 
 public class Main {
 
+	public static final String MAIN_WINDOW_NAME = null;
+	public static final String SNIPER_STATUS_NAME = null;
+
 	public static void main(String xmppHostname, String sniperId,
 			String sniperPassword, Object itemId) {
 		// TODO Auto-generated method stub
--- src/test/endtoend/actionsniper/AuctionSniperDriver.java
+++ src/test/endtoend/actionsniper/AuctionSniperDriver.java
@@ -1,19 +1,32 @@
 package test.endtoend.actionsniper;
 
-public class AuctionSniperDriver {
+import actionsniper.Main;
 
-	public AuctionSniperDriver(int i) {
-		// TODO Auto-generated constructor stub
-	}
+import com.objogate.wl.swing.AWTEventQueueProber;
+import com.objogate.wl.swing.driver.JFrameDriver;
+import com.objogate.wl.swing.driver.JLabelDriver;
+import com.objogate.wl.swing.gesture.GesturePerformer;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class AuctionSniperDriver extends JFrameDriver {
+
+	  public AuctionSniperDriver(int timeoutMillis) { 
+		    super(new GesturePerformer(), 
+		          JFrameDriver.topLevelFrame( 
+		            named(Main.MAIN_WINDOW_NAME), 
+		            showingOnScreen()), 
+		            new AWTEventQueueProber(timeoutMillis, 100)); 
+		  }
 
 	public void dispose() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	public void showsSniperStatus(String statusJoining) {
-		// TODO Auto-generated method stub
-		
-	}
+	  public void showsSniperStatus(String statusText) {
+	    new JLabelDriver(this, 
+	    		named(Main.SNIPER_STATUS_NAME)).hasText(equalTo(statusText));
+	  }
 
 }
