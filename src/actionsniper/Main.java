c3acf5ebd1128bdf5dbad153b95ee5e59b65c69b
kgeneral <kgeneral@hanmail.net>
2014-08-25 20:52:39 +0900

    add chat impl

--- src/actionsniper/Main.java
+++ src/actionsniper/Main.java
@@ -7,7 +7,25 @@ import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
 import javax.swing.border.LineBorder;
 
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Message;
+
 public class Main {
+
+	private static final int ARG_HOSTNAME = 0;
+	private static final int ARG_USERNAME = 1;
+	private static final int ARG_PASSWORD = 2;
+
+	private static final int ARG_ITEM_ID = 3;
+
+	public static final String AUCTION_RESOURCE = "Auction";
+	public static final String ITEM_ID_AS_LOGIN = "auction-%s";
+
+	public static final String AUCTION_ID_FORMAT = ITEM_ID_AS_LOGIN + "@%s/"
+			+ AUCTION_RESOURCE;
 
 	public static final String MAIN_WINDOW_NAME = "Auction Sniper Main";
 	public static final String SNIPER_STATUS_NAME = "sniper status";
@@ -20,6 +38,35 @@ public class Main {
 
 	public static void main(String... args) throws Exception {
 		Main main = new Main();
+		XMPPConnection connection = connectTo(args[ARG_HOSTNAME],
+				args[ARG_USERNAME], args[ARG_PASSWORD]);
+
+		Chat chat = connection.getChatManager().createChat(
+				auctionId(args[ARG_ITEM_ID], connection),
+				new MessageListener() {
+
+					@Override
+					public void processMessage(Chat aChat, Message message) {
+						// TODO Auto-generated method stub
+
+					}
+				});
+
+		chat.sendMessage(new Message());
+	}
+
+	private static String auctionId(String itemId, XMPPConnection connection) {
+		return String.format(AUCTION_ID_FORMAT, itemId,
+				connection.getServiceName());
+	}
+
+	private static XMPPConnection connectTo(String hostname, String username,
+			String password) throws XMPPException {
+		XMPPConnection connection = new XMPPConnection(hostname);
+		connection.connect();
+		connection.login(username, password, AUCTION_RESOURCE);
+
+		return connection;
 	}
 
 	private void startUserInterface() throws Exception {
@@ -31,15 +78,15 @@ public class Main {
 	}
 
 	public class MainWindow extends JFrame {
-		
+
 		private static final long serialVersionUID = -4274409492013695538L;
-		
+
 		public static final String SNIPER_STATUS_NAME = "sniper status";
 
 		private static final String STATUS_JOINING = "Joining";
-		
+
 		private final JLabel sniperStatus = createLabel(STATUS_JOINING);
-		
+
 		public MainWindow() {
 			super("Auction Sniper");
 			setName(MAIN_WINDOW_NAME);
@@ -55,8 +102,7 @@ public class Main {
 			result.setBorder(new LineBorder(Color.BLACK));
 			return result;
 		}
-		
-		
+
 	}
 
 }
